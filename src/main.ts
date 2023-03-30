7669e3381215ad405dd62c74dc244be0c3e2474e
Kilian McMahon <kilmc@fastmail.com>
2023-03-30 17:00:42 +0200

    docs: add jsdocs comment

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+/**
+ * A function that adds two numbers
+ * @param {number} first - First number
+ * @param {number} second - Second number
+ */
 export function add(first: number, second: number) {
   return first + second;
 }
